8d177705ff77133570231da692aa28777461489c
weiyialanchen <weiyi.alan.chen@gmail.com>
2015-04-30 10:50:39 -0400

    tutorial for local data

---
+++ Example/local_dataAccess.py
@@ -0,0 +1,57 @@
+'''
+(c) 2015 Baruch College - AlgoTrading course
+@summary: An example to show how dataAccess works.
+'''
+
+# QSTK Imports
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkutil.DataAccess as da
+
+# Third Party Imports
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def main():
+    ''' Main Function'''
+    # Creating an object of DataAccess Class
+    c_dataobj = da.DataAccess('Yahoo')
+
+    # Getting a list of symbols from Lists
+    # Lists : S&P5002012, S&P5002008, Index
+    ls_symbols = c_dataobj.get_symbols_from_list('sp5002012')
+    print "Symbols from the list : ", ls_symbols
+
+    # All symbols possible
+    ls_all_syms = c_dataobj.get_all_symbols()
+    print "All symbols : ", ls_all_syms
+
+    ls_syms_toread = ['AAPL', 'GOOG']
+
+    # List of TimeStamps to read
+    ldt_timestamps = []
+    ldt_timestamps.append(dt.datetime(2010, 10, 14, 16))
+    ldt_timestamps.append(dt.datetime(2010, 10, 15, 16))
+    ldt_timestamps.append(dt.datetime(2010, 11, 21, 16))
+    ldt_timestamps.append(dt.datetime(2010, 11, 22, 16))
+    ldt_timestamps.append(dt.datetime(2010, 11, 23, 16))
+    ldt_timestamps.append(dt.datetime(2010, 11, 24, 16))
+    ldt_timestamps.append(dt.datetime(2010, 11, 25, 16))
+    ldt_timestamps.append(dt.datetime(2010, 11, 26, 16))
+    ldt_timestamps.append(dt.datetime(2010, 11, 27, 10))
+    ldt_timestamps.append(dt.datetime(2010, 11, 27, 16))
+    ldt_timestamps.append(dt.datetime(2020, 11, 27, 16))
+    ldt_timestamps.append(dt.datetime(2020, 11, 27, 18))
+
+    # Reading the data
+    # By default it'll read data from the default data provided,
+    # But a path can be provided using either an environment variable or
+    # as a prarameter.
+    df_close = c_dataobj.get_data(ldt_timestamps, ls_syms_toread, "close")
+    print df_close
+
+
+if __name__ == '__main__':
+    main()
